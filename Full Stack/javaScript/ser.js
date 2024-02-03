2fdcd84139e7501248cd463eacb51fbcbc83ddca
Pavan Wakade <130147767+Pavan884455@users.noreply.github.com>
2024-02-03 11:28:07 +0530

    Add files via upload

---
+++ Full Stack/javaScript/ser.js
@@ -0,0 +1,8 @@
+var http=require("http");
+
+http.createServer(function (request,respons)
+{
+response.writeHead(200,{'Content-Type':'text/plain'});
+response.end('HELLO NODEJS\n');
+}).listen(8081);
+console.log('server running at http://127.0.0.1:8081/');
